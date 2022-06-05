697ee69b7e715335a80083f292aa6e804a34bc8e
AjAutomation <AjAutomation09@gmail.com>
2022-06-05 17:04:39 +0530

    jenkins parameters project

---
+++ src/test/java/tests/test1.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+
+	@Test
+	public void testcase1() {
+		System.out.println("#########################################");
+		System.out.println("#      Jenkins Parameters Session       #");
+		System.out.println("#########################################");
+
+	}
+}
